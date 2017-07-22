292ec542c92535a7081e11ea9841e111dac801ff
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-22 09:59:43 -0700

    lk

--- todolistapi/app.js
+++ todolistapi/app.js
@@ -7,4 +7,13 @@ var db = require('./db');
 var RequestController = require('./requests/RequestController');
 app.use('/requests', RequestController);
 
+var UserController = require('./user/UserController');
+app.use('/users', UserController);
+
+(deleteoldrequests(){
+    // do some stuff
+    console.log("delete")
+    setTimeout(arguments.callee, 60000);
+})();
+
 module.exports = app;
