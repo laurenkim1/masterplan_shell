e3941c62ce7115e3ece47790414d04e10237e571
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-26 20:42:11 -0700

    facebook login

---
+++ masterplan/masterplan/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LogInViewController.swift
+//  masterplan
+//
+//  Created by Lauren Kim on 7/26/17.
+//  Copyright © 2017 Lauren Kim. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FacebookCore
+import FacebookLogin
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let accessToken = AccessToken.current {
+            // User is logged in, use 'accessToken' here.
+            // User is logged in, do work such as go to next view controller.
+            performSegue(withIdentifier: "loggedIn", sender: self)
+        }
+        
+        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        let navVc = segue.destination as! TabBarController
+    }
+    
+
+}
