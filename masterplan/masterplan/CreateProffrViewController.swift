63210e86b5845879472b37c423700edf84dda020
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-27 22:33:09 -0700

    lk

--- masterplan/masterplan/CreateProffrViewController.swift
+++ masterplan/masterplan/CreateProffrViewController.swift
@@ -39,14 +39,17 @@ class CreateProffrViewController: UIViewController {
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // MARK: Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        super.prepare(for: segue, sender: sender)
+        
+        if let channel = sender as? ProffrChannel {
+            let chatVc = segue.destination as! ChatViewController
+            
+            chatVc.senderDisplayName = senderDisplayName
+            chatVc.channel = channel
+            chatVc.channelRef = channelRef.child(channel.id)
+        }
     }
-    */
 
 }
