f215a71e1da122182737989612c5e2816afa41e4
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-28 21:50:34 -0700

    create proffer view

--- masterplan/masterplan/CreateProffrViewController.swift
+++ masterplan/masterplan/CreateProffrViewController.swift
@@ -9,25 +9,81 @@
 import UIKit
 import Firebase
 
-class CreateProffrViewController: UIViewController {
+class CreateProffrViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     private lazy var channelRef: DatabaseReference = Database.database().reference().child("channels")
     private var channelRefHandle: DatabaseHandle?
     var senderDisplayName: String?
     var request: requestInfo?
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Handle the text field’s user input through delegate callbacks.
+        messageTextField.delegate = self
     }
     
-    // MARK :Actions
+    //MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        // createProffrButton.isEnabled = false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // updateCreateButtonState()
+        navigationItem.title = textField.text
+    }
+
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard.
+        messageTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
     @IBAction func createProffr(_ sender: UIButton) {
         if let subTitle = request?.requestTitle { // username actually
             let newChannelRef = channelRef.childByAutoId() // 2
@@ -62,6 +118,14 @@ class CreateProffrViewController: UIViewController {
             chatVc.channelRef = channelRef.child(channel.id)
             chatVc.hidesBottomBarWhenPushed = true
         }
+    }
+    
+    //MARK: Private Methods
+    
+    private func updateCreateProffrButtonState() {
+        // Disable the Save button if the text field is empty.
+        let text = messageTextField.text ?? ""
+        // createProffrButton.isEnabled = !text.isEmpty
     }
 
 }
