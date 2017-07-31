73f9146a5e07b303c538105a33bca5c689bfb764
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-30 19:28:39 -0700

    thank you jesus

--- todolistapi/db.js
+++ todolistapi/db.js
@@ -1,9 +1,11 @@
 // db.js
+
 var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 var options = {
 	useMongoClient: true
 };
 var promise = mongoose.connect('mongodb://laurenkim:jihye197@ds163232.mlab.com:63232/requests', options);
-promise.then(function (db) {
-	db.requests.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 10 } );
+promise.then( function (db) {
+	db.collection("requests").createIndex( { "createdAt": 1 }, { expireAfterSeconds: 10 } );
 });
